57cf35266d501fd8f158812eb81336e9bebe7cb7
Eman-Saleh151 <emansalehsaleeh@gmail.com>
2025-09-29 10:14:52 +0300

    chore: initial project skeleton with tailwind and basic setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div  class="bg-mint-500">
+    <!--   Navbar هنا -->
+    <router-view /> <!-- دي هتعرض صفحة Home -->
+    <!--   Footer هنا -->
+  </div>
+
+</template>
+
+
